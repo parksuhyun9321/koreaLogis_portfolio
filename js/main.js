e33198dd4f31f1a99d95d48bd588cabbc478c4cf
parksuhyun9321 <59465364+parksuhyun9321@users.noreply.github.com>
2020-07-21 15:22:41 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,17 +6,28 @@ new fullpage ("#main",{
             section01.restart();
         }
     }
-})
-
-
-let section01 = gsap.timeline();
-section01.from("#section01 .txtBox .main",{
-    duration:0.5,
-    y:50,
-    opacity:0,
-    ease:"back.in"
 });
 
+// fullpage 
+
+$(window).on("resize",function(){
+    let w = $(window).width() + 17;
+    // console.log("w", w)
+    if(w<=1200){
+        if(!$("body").hasClass("mobile")){
+            $("body").addClass("mobile");
+        }   
+    }  else {
+        if( $("body").hasClass("mobile")){
+            $("body").removeClass("mobile");
+        }
+        $("body").removeClass("open");
+        $(".utilMenu").removeClass("open");
+    }
+});
+$(window).trigger("resize");
+
+// 웹화면의 사이즈에 변화가 있을때의 이벤트
 
 $("#gnb .list > li").on("mouseenter",function(){
     if(!$("#gnb").hasClass("mobile")){
@@ -29,39 +40,25 @@ $("#gnb .list > li").on("mouseleave",function(){
     }
 });
 
+// navigation 
+
 
 $("#gnb .list > li .depth01").on("click",function(){
     $(this).next(".depth02").stop().slideToggle();
     $(this).parent().siblings().find(".depth02").stop().slideUp();
     return false;
-})
-
-
-$(window).on("resize",function(){
-    let w = $(window).width() + 17;
-    // console.log("w", w)
-    if(w<=1200){
-        if( !$("#gnb").hasClass("mobile")){
-        $("#gnb").addClass("mobile");
-        }   
-    }  else {
-        if( $("#gnb").hasClass("mobile")){
-            $("#gnb").removeClass("mobile");
-        }
-        $("body").removeClass("open");
-        $(".utilMenu").removeClass("open");
-    }
 });
-$(window).trigger("resize");
 
-
-
+//  mobile-navigation 
 
 $(".utilMenu").on("click",function(){
     $(this).toggleClass("open");
     $("body").toggleClass("open");
     return false;
 });
+
+// mobile-utill menu
+
 $(".btnSearch").on("click",function(){
     if ($("body").addClass("overHidden")){
         $(".searchBar").fadeIn(250);
@@ -70,8 +67,25 @@ $(".btnSearch").on("click",function(){
     }
     return false;
 });
+
 $(".searchBar .btnClose").on("click",function(){
     $(".searchBar").fadeOut(250);
     $("body").removeClass("overHidden");
     return false;
-})
+});
+
+// click 시 검색창 구현/닫기
+
+// #header
+
+let section01 = gsap.timeline();
+section01.from("#section01 .txtBox .main",{
+    duration:0.5,
+    y:50,
+    opacity:0,
+    ease:"back.in"
+});
+
+// greensock 
+
+// #section01
